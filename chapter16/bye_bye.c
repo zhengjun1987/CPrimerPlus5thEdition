ae318b71df13745dc61b3d0dc34b8e5a7ca4434f
郑军 <zhengjun1987@outlook.com>
2018-03-19 23:57:31 +0700

    《C Primer Plus》第五版 第16章 C预处理器和C库 2018-03-19 23:57:24

---
+++ chapter16/bye_bye.c
@@ -0,0 +1,7 @@
+/**
+ *  Author:Zheng Jun
+ *  Date:2018/3/20 00:53
+ *  E-mail:zhengjun1987@outlook.com
+ *  Desc:
+ */
+
---
+++ chapter16/rect_pol.c
@@ -0,0 +1,26 @@
+/**
+ *  Author:Zheng Jun
+ *  Date:2018/3/20 00:52
+ *  E-mail:zhengjun1987@outlook.com
+ *  Desc:
+ */
+#include <math.h>
+#include <stdio.h>
+#define RAD_TO_DEG (180/(4*atan(1)))
+
+typedef struct polar_v {
+    double magnitude;
+    double angle;
+} POLAR_V;
+
+typedef struct rect_v {
+    double x;
+    double y;
+} RECT_V;
+
+
+
+int main(){
+
+    return 0;
+}
